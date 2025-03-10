96f6ea5044ed1ad1f4d7b4ca5c75d572e6f98025
Amaris <amarisosorio10@gmail.com>
2025-03-10 11:09:31 -0600

    Ya esta el crud

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,16 @@
 //Importo todo lo de la libreria express
 import express from "express";
+import productsRoutes from "./src/routes/products.js" //OJO: recuerda siempre agregar el archivo '.js'
 
-//Crear una constante que es igual
-// a la libreria que importe y la ejecutamos
+/*Crear una constante que es igual
+ a la libreria que importe y la ejecutamos*/
 const app = express();
 
-//Definir la ruta
-app.use("/api/products")
 
+app.use(express.json()); //Esto permitira el uso de middleware para que acepte datos json, se coloca siempre arriba de la ruta
+
+//Definir la ruta
+app.use("/api/products", productsRoutes)
 
 
 //Exportó la constante para poder usar express en otros lados
--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -4,9 +4,16 @@ que tiene la ruta "/api/products"
 */
 
 import express from "express";
+import productsController from "../controllers/productsController.js"; //OJO: recuerda siempre agregar el archivo '.js'
 
 const router = express.Router();
 
-router.route("/").get().post().put().delete()
+router.route("/") //Nota: sin id
+.get(productsController.getProducts)
+.post(productsController.insertProducts);
+
+router.route("/:id") //Nota: con id
+.put(productsController.updateProduct)
+.delete(productsController.deleteProduct);
 
 export default router;
